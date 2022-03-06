3fe408c91d51c27efc0006d687e9a8c128d5c741
choi0125 <choi0125@algonquinlive.com>
2022-03-06 15:42:51 -0500

    Added POST route logic for student resource path

--- routes/students.js
+++ routes/students.js
@@ -13,3 +13,27 @@ router.get('/:studentId', async (req, res) => {
   let student = await Student.findById(id)
   res.send({data: formatResponseData('students', student.toObject())})
 });
+
+router.post('/', async (req, res)=>{
+  // 1. get data from req.body
+  const attributes = req.body.data.attributes
+  const type = req.body.data.type
+  // 2. check type
+  if (type === 'students') {
+    let newStudent = new Student(attributes)
+
+  await newStudent.save()
+
+    res.status(201).json({ data: formatResponseData('students', newStudent._doc)})
+  } else {
+    res.status(400).json({
+      errors: [
+        {
+          status: '400',
+          title: 'invalid type',
+          message: `expected type student but received ${data?.type}`
+        }
+      ]
+    })
+  }
+})
