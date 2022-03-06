578fe677f87d50f67e69ff9fa3d738527194b71a
choi0125 <choi0125@algonquinlive.com>
2022-03-06 15:38:54 -0500

    set up server

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // Don't forget to use NPM to install Express and Mongoose.
+
+'use strict'
+
+//load dependecies
+const morgan = require('morgan');
+const express = require('express');
+const studentsRouter = require('./routes/students');
+const coursesRouter = require('./routes/courses');
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true})
+
+
+const app = express();
+
+app.use( morgan('tiny') );
+app.use( express.json() );
+
+app.use('/api/students', studentsRouter);
+app.use('/api/courses', coursesRouter);
+
+const port = process.env.port || 3030;
+app.listen(port, () => console.log(`Server listening on port ${port} ...`))
