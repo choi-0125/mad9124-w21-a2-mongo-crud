eb71a63505b7c50be6b4cb9362fe310eedb7a126
choi0125 <choi0125@algonquinlive.com>
2022-03-06 15:40:13 -0500

    Created Class Model

---
+++ models/Course.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const schema = new mongoose.Schema({
+  code: {type: String, required: true, max: 16},
+  title: {type: String, required: true, max: 255},
+  description: {type: String, max: 2048},
+  url: {type: String, max: 512},
+  students: {type: [mongoose.Schema.Types.ObjectId], ref: 'Student'}
+})
+
+const Model = mongoose.model('Course', schema )
+
+module.export(Model)
