7561f17c405f59707f55dca3ce956e45726025b2
choi0125 <choi0125@algonquinlive.com>
2022-03-07 11:32:47 -0500

    refactored sanitizeBody middleware to recursively call itself for Objects and Array types

--- middleware/sanitizeBody.js
+++ middleware/sanitizeBody.js
@@ -1,15 +1,26 @@
 const debug = require('debug')('sanitize:body')
 const xss = require('xss')
 
+const sanitize = sourceString => {
+  return xss(sourceString, {
+        whiteList: [],
+        stripIgnoreTag: true,
+        stripIgnoreTagBody: ['script']
+      })
+}
+
 const stripTags = payload => {
-  console.log(payload)
   let attributes = {...payload}
   for(let key in attributes){
-    attributes[key] = xss(attributes[key], {
-      whiteList: [],
-      stripIgnoreTag: true,
-      stripIgnoreTagBody: ['script']
-    })
+    if (attributes[key] instanceof Object){
+      attributes[key] = stripTags(attributes[key])
+    } else if (attributes[key] instanceof Array) {
+      attributes[key] = attributes[key].map( element => {
+        return typeof element === 'string' ? sanitize(element) : stripTags(element)
+      })
+    } else {
+      attributes[key] = sanitize(attributes[key])
+    }
   }
   return attributes
 }
