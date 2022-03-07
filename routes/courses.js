19582b635bcf3532b8c588f8ed85986b70f6e8b4
choi0125 <choi0125@algonquinlive.com>
2022-03-06 22:18:17 -0500

    Added POST logic for courses router

--- routes/courses.js
+++ routes/courses.js
@@ -12,4 +12,33 @@ router.get('/', async (req, res) => {
   })
 })
 
+router.post('/', async (req, res)=>{
+  // 1. get data from req.body
+  const attributes = req.body.data.attributes
+  const type = req.body.data.type
+  // 2. check type
+  if (type === 'courses') {
+    let newCourse = new Course(attributes)
+
+  await newCourse.save()
+
+    res.status(201).json({ data: formatResponseData('courses', newCourse._doc)})
+  } else {
+    res.status(400).json({
+      errors: [
+        {
+          status: '400',
+          title: 'invalid type',
+          message: `expected type course but received ${data?.type}`
+        }
+      ]
+    })
+  }
+})
+
+function formatResponseData(type, res) {
+  const {_id, ...attributes} = res;
+  return {type, id: _id, attributes};
+}
+
 module.exports = router
